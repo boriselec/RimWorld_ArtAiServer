cc5edabcb8e618336243eb8751f378c0bffc02f7
boriselec <boris.puzikov@gmail.com>
2023-03-17 22:35:11 +0300

    enh: crib unrelated desc cleanup

--- src/main/java/com/boriselec/rimworld/aiart/ArtDescriptionTextProcessor.java
+++ src/main/java/com/boriselec/rimworld/aiart/ArtDescriptionTextProcessor.java
@@ -5,6 +5,7 @@ import com.boriselec.rimworld.aiart.data.Request;
 public class ArtDescriptionTextProcessor {
     private static final String TALE_DATE_REGEX = " on [^ ]+ of (Aprimay|Jugust|Septober|Decembary)[^.]+.";
     private static final String DRESSER_UNRELATED = "Placing more than one dresser near the same bed has no effect\\. ";
+    private static final String CRIB_UNRELATED = "A high quality crib will make a baby happy\\. Multiple cribs can be placed in a bedroom without turning it into a barracks\\. ";
     // some modes adds info in <i> tags
     private static final String ITALIC_TAG = "<i>.*</i>";
     // (Vanilla Furniture Expanded - Art)
@@ -14,6 +15,7 @@ public class ArtDescriptionTextProcessor {
         String description = request.thingDesc() + " " + request.artDesc();
         return description.replaceAll(TALE_DATE_REGEX, ".")
                 .replaceAll(DRESSER_UNRELATED, "")
+                .replaceAll(CRIB_UNRELATED, "")
                 .replaceAll(ITALIC_TAG, "")
                 .replaceAll(MOD_FURNITURE, "")
                 .replaceAll("\\n", "")
