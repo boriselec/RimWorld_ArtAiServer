5cd357738bc92a1ec9981307268709a9ca169a29
boriselec <boris.puzikov@gmail.com>
2023-03-20 19:33:29 +0300

    enh: generalize vanilla expanded exclusion rule

--- src/main/java/com/boriselec/rimworld/aiart/ArtDescriptionTextProcessor.java
+++ src/main/java/com/boriselec/rimworld/aiart/ArtDescriptionTextProcessor.java
@@ -8,8 +8,8 @@ public class ArtDescriptionTextProcessor {
     private static final String CRIB_UNRELATED = "A high quality crib will make a baby happy\\. Multiple cribs can be placed in a bedroom without turning it into a barracks\\. ";
     // some modes adds info in <i> tags
     private static final String ITALIC_TAG = "<i>.*</i>";
-    // (Vanilla Furniture Expanded - Art)
-    private static final String MOD_FURNITURE = "\\(Vanilla Furniture Expanded - Art\\)";
+    // Vanilla Expanded
+    private static final String VANILLA_EXPANDED = "\\(Vanilla [^)]*Expanded[^)]*\\)";
 
     public static String getDescription(Request request) {
         String description = request.thingDesc() + " " + request.artDesc();
@@ -17,7 +17,7 @@ public class ArtDescriptionTextProcessor {
                 .replaceAll(DRESSER_UNRELATED, "")
                 .replaceAll(CRIB_UNRELATED, "")
                 .replaceAll(ITALIC_TAG, "")
-                .replaceAll(MOD_FURNITURE, "")
+                .replaceAll(VANILLA_EXPANDED, "")
                 .replaceAll("\\n", "")
                 .replaceAll("  +", " ");
     }
