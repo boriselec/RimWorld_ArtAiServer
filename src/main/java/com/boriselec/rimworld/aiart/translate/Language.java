5d0f10dc555067cc26916ce7e519487b087aca6d
boriselec <boris.puzikov@gmail.com>
2023-07-23 21:48:04 +0300

    enh: accept plain language description

--- src/main/java/com/boriselec/rimworld/aiart/translate/Language.java
+++ src/main/java/com/boriselec/rimworld/aiart/translate/Language.java
@@ -1,54 +1,61 @@
 package com.boriselec.rimworld.aiart.translate;
 
+import org.springframework.util.StringUtils;
+
 import java.util.Arrays;
 
 public enum Language {
-    CATALAN("Catalan (Català)", "ca"),
-    CHINESE_SIMPLIFIED("ChineseSimplified (简体中文)", "zh-CN"),
-    CHINESE_TRADITIONAL("ChineseTraditional (繁體中文)", "zh-TW"),
-    CZECH("Czech (Čeština)", "cs"),
-    DANISH("Danish (Dansk)", "da"),
-    DUTCH("Dutch (Nederlands)", "nl"),
-    ENGLISH("English", "en"),
-    ESTONIAN("Estonian (Eesti)", "et"),
-    FINNISH("Finnish (Suomi)", "fi"),
-    FRENCH("French (Français)", "fr"),
-    GERMAN("German (Deutsch)", "de"),
-    GREEK("Greek (Ελληνικά)", "el"),
-    HUNGARIAN("Hungarian (Magyar)", "hu"),
-    ITALIAN("Italian (Italiano)", "it"),
-    JAPANESE("Japanese (日本語)", "ja"),
-    KOREAN("Korean (한국어)", "ko"),
-    NORWEGIAN("Norwegian (Norsk Bokmål)", "no"),
-    POLISH("Polish (Polski)", "pl"),
-    PORTUGUESE("Portuguese (Português)", "pt"),
-    PORTUGUESE_BRAZILIAN("PortugueseBrazilian (Português Brasileiro)", "pt"),
-    ROMANIAN("Romanian (Română)", "ro"),
-    RUSSIAN("Russian (Русский)", "ru"),
-    RUSSIAN_SK("Russian-SK", "ru"),
-    SLOVAK("Slovak (Slovenčina)", "sk"),
-    SPANISH("Spanish (Español(Castellano))", "es"),
-    SPANISH_LATIN("SpanishLatin (Español(Latinoamérica))", "es"),
-    SWEDISH("Swedish (Svenska)", "sv"),
-    TURKISH("Turkish (Türkçe)", "tr"),
-    UKRAINIAN("Ukrainian (Українська)", "uk"),
-    VIETNAMESE("Vietnamese", "vi"),
+    CATALAN("Catalan (Català)", "Catalan", "ca"),
+    CHINESE_SIMPLIFIED("ChineseSimplified (简体中文)", "Chinese", "zh-CN"),
+    CHINESE_TRADITIONAL("ChineseTraditional (繁體中文)", null, "zh-TW"),
+    CZECH("Czech (Čeština)", "Czech", "cs"),
+    DANISH("Danish (Dansk)", "Danish", "da"),
+    DUTCH("Dutch (Nederlands)", "Dutch", "nl"),
+    ENGLISH("English", "English", "en"),
+    ESTONIAN("Estonian (Eesti)", "Estonian", "et"),
+    FINNISH("Finnish (Suomi)", "Finnish", "fi"),
+    FRENCH("French (Français)", "French", "fr"),
+    GERMAN("German (Deutsch)", "German", "de"),
+    GREEK("Greek (Ελληνικά)", "Greek", "el"),
+    HUNGARIAN("Hungarian (Magyar)", "Hungarian", "hu"),
+    ITALIAN("Italian (Italiano)", "Italian", "it"),
+    JAPANESE("Japanese (日本語)", "Japanese", "ja"),
+    KOREAN("Korean (한국어)", "Korean", "ko"),
+    NORWEGIAN("Norwegian (Norsk Bokmål)", "Norwegian", "no"),
+    POLISH("Polish (Polski)", "Polish", "pl"),
+    PORTUGUESE("Portuguese (Português)", "Portuguese", "pt"),
+    PORTUGUESE_BRAZILIAN("PortugueseBrazilian (Português Brasileiro)", null, "pt"),
+    ROMANIAN("Romanian (Română)", "Romanian", "ro"),
+    RUSSIAN("Russian (Русский)", "Russian", "ru"),
+    RUSSIAN_SK(null, "Russian-SK", "ru"),
+    SLOVAK("Slovak (Slovenčina)", "Slovak", "sk"),
+    SPANISH("Spanish (Español(Castellano))", "Spanish", "es"),
+    SPANISH_LATIN("SpanishLatin (Español(Latinoamérica))", null, "es"),
+    SWEDISH("Swedish (Svenska)", "Swedish", "sv"),
+    TURKISH("Turkish (Türkçe)", "Turkish", "tr"),
+    UKRAINIAN("Ukrainian (Українська)", "Ukrainian", "uk"),
+    VIETNAMESE(null, "Vietnamese", "vi"),
     ;
 
     /**
      * Language folder (Data/Core/Languages)
      */
     private final String rimworldCode;
+    private final String englishName;
     private final String iso639Code;
 
-    Language(String rimworldCode, String iso639Code) {
+    Language(String rimworldCode, String englishName, String iso639Code) {
         this.rimworldCode = rimworldCode;
+        this.englishName = englishName;
         this.iso639Code = iso639Code;
     }
 
     public static Language fromRimworldCode(String code) {
+        if (!StringUtils.hasText(code)) {
+            return ENGLISH;
+        }
         return Arrays.stream(values())
-                .filter(l -> l.rimworldCode.equals(code))
+                .filter(lang -> code.equals(lang.rimworldCode) || code.equals(lang.englishName))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Unsupported language: " + code));
     }
