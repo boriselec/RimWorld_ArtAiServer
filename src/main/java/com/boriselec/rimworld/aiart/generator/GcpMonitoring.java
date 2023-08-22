88d126b63b668f5fab723c9cbd3099e2b90d44ff
boriselec <boris.puzikov@gmail.com>
2023-08-22 18:36:45 +0300

    monitoring: gcp status

--- src/main/java/com/boriselec/rimworld/aiart/generator/GcpMonitoring.java
+++ src/main/java/com/boriselec/rimworld/aiart/generator/GcpMonitoring.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 @ConditionalOnProperty("gcp.project")
@@ -15,17 +15,24 @@ public class GcpMonitoring {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final GcpClient gcpClient;
 
-    private final AtomicBoolean isRunning = new AtomicBoolean(false);
+    private final AtomicReference<String> status = new AtomicReference<>("");
 
     public GcpMonitoring(GcpClient gcpClient, MeterRegistry meterRegistry) {
         this.gcpClient = gcpClient;
-        meterRegistry.gauge("gcp.running", isRunning, isRunning -> isRunning.get() ? 1 : 0);
+        meterRegistry.gauge("gcp.status", status, status -> switch (status.get()) {
+            default -> 0;
+            case "PROVISIONING" -> 1;
+            case "STAGING" -> 2;
+            case "RUNNING" -> 3;
+            case "STOPPING" -> 4;
+            case "TERMINATED" -> 5;
+        });
     }
 
     @Scheduled(fixedRate = 1000)
     public void process() {
-        String status = gcpClient.get().getStatus();
-        log.info("gcp status: " + status);
-        isRunning.set("RUNNING".equals(status));
+        String currentStatus = gcpClient.get().getStatus();
+        log.info("gcp status: " + currentStatus);
+        status.set(currentStatus);
     }
 }
